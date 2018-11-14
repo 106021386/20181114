cd6aa7c1fd612d76472cb251887d287748489ca3
I432 <11>
2018-11-14 14:31:21 +0800

    1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+                Scanner scn = new Scanner(System.in);
+                String a = scn.next();
+                int b=0,c=0;
+                for(int i =0;i<a.length();i++){
+                    b=(int) a.charAt(i)-(int)'0';
+                    c+=b;
+                }
+                System.out.println(c);
+
+
+            }
+        }
+    }
+}
